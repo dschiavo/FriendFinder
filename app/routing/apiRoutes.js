f28e39664c52217e59a73a2d2e4b855dcb86ef8c
RBG <dschiavo.262@gmail.com>
2019-01-02 20:49:19 -0500

    final for now

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -17,3 +17,11 @@ module.exports = function(app) {
    
     })
 };
+
+//steps left to do:
+//1. fix the submit button so that when it is clicked, data is stored
+//2. convert answers to an array of numbers
+//3. compare difference between users' scores (get the absolute value)
+//4. add up difference to calculate Total Difference
+//5. smallest totalDifference = friend
+//6. display name and pic of friend
